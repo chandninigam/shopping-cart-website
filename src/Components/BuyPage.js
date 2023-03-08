9a619b36298aafff0cb367b838f31a4e58f03bdd
chandni nigam <nigamchandni139@gmail.com>
2023-03-09 03:22:23 +0530

    Calling Pexel API

--- src/Components/BuyPage.js
+++ src/Components/BuyPage.js
@@ -4,42 +4,50 @@ import CardItem from "./CardItem";
 import { random, commerce } from "faker";
 import { Container, Col, Row } from "reactstrap";
 
-const localurl = "https://api.jsonbin.io/b/5ee62ec719b60f7aa959d69c/1";
+const apiKey = "ImXToOb1wT04vEos8rC8JFVLdQvCxTeadJCIHCGF5JoFazuhzemmlTbA";
+
+const url = "https://api.pexels.com/v1/search?query=laptop&per_page=6&page=1";
 
 const BuyPage = ({ addItem }) => {
-	const [product, setproduct] = useState([]);
+  const [product, setproduct] = useState([]);
 
-	const fetchphotos = async () => {
-		const { data } = await Axios.get(localurl);
+  const fetchphotos = async () => {
+    const { data } = await Axios.get(url, {
+      headers: {
+        Authorization: apiKey,
+      },
+    });
 
-		const { photos } = data;
+    console.log(data);
 
-		const allProduct = photos.map((photos) => ({
-			smallimage: photos.src.medium,
-			tinyimage: photos.src.tiny,
-			productName: random.word(),
-			productPrice: commerce.price(),
-			id: random.uuid(),
-		}));
+    const { photos } = data;
 
-		setproduct(allProduct);
-	};
+    const allProduct = photos.map((photos) => ({
+      smallimage: photos.src.medium,
+      tinyimage: photos.src.tiny,
+      productName: random.word(),
+      productPrice: commerce.price(),
+      id: random.uuid(),
+    }));
 
-	useEffect(() => {
-		fetchphotos();
-	}, []);
+    setproduct(allProduct);
+  };
 
-	return (
-		<Container fluid>
-			<h1 className="text-success text-center">Buy NOW</h1>
-			<Row>
-				{product.map((product) => (
-					<Col md={4} key={product.id}>
-						<CardItem product={product} addItem={addItem} />
-					</Col>
-				))}
-			</Row>
-		</Container>
-	);
+  useEffect(() => {
+    fetchphotos();
+  }, []);
+
+  return (
+    <Container fluid>
+      <h1 className="text-success text-center">Buy NOW</h1>
+      <Row>
+        {product.map((product) => (
+          <Col md={4} key={product.id}>
+            <CardItem product={product} addItem={addItem} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 };
 export default BuyPage;
